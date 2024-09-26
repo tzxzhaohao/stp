1538c6f3a2154a816a73afe312f43310bfba2936
zhaohao4 <1104270550@qq.com>
2024-09-26 16:36:22 +0800

    修复bug

--- src/views/Comprehensive/Comprehensive.vue
+++ src/views/Comprehensive/Comprehensive.vue
@@ -14,7 +14,7 @@ import MenuList from '@/views/Comprehensive/components/MenuList.vue'
 import WaterQuality from '@/views/Comprehensive/waterQuality.vue'
 import { onMounted } from 'vue'
 const loadCesium3Dtileset = async (url: string) => {
-  const { viewer, Cesium3DTileset, Cartographic, Cartesian3, Matrix4 } = window.Cesium.viewer
+  const { viewer, Cesium3DTileset, Cartographic, Cartesian3, Matrix4 } = window.Cesium
   const tileset = new Cesium3DTileset({
     url,
     maximumScreenSpaceError: 16,
@@ -27,6 +27,7 @@ const loadCesium3Dtileset = async (url: string) => {
   const offset = Cartesian3.fromRadians(cartographic.longitude, cartographic.latitude, 17)
   const translation = Cartesian3.subtract(offset, surface, new Cartesian3())
   layer.modelMatrix = Matrix4.fromTranslation(translation)
+  viewer.flyTo(tileset)
 }
 onMounted(() => {
   if (window.Cesium) {
