a403e025c74a4e683dcae98a796c992cd62460e4
zhaohao4 <1104270550@qq.com>
2025-01-08 16:06:12 +0800

    fix: 替换typed-subtitle.ts文件

--- src/common/typed-subtitle.ts
+++ src/common/typed-subtitle.ts
@@ -1,23 +1,26 @@
 import Typed from 'typed.js'
 
+const isMicApp = window.__MICRO_APP_ENVIRONMENT__
+
 export function createSubtitleBar(viewer: any) {
   const container = viewer.container
 
-  const statusbarContainer = document.createElement('div')
+  const statusbarContainer = isMicApp ? window.microApp.pureCreateElement('div') : document.createElement('div')
   statusbarContainer.className = 'mega3d-subtitle'
   statusbarContainer.style.left = '0px'
   statusbarContainer.style.right = '0px'
   statusbarContainer.style.bottom = '0px'
   container.appendChild(statusbarContainer)
 
-  const statusbarContent = document.createElement('span')
+  const statusbarContent = isMicApp ? window.microApp.pureCreateElement('span') : document.createElement('span')
   statusbarContent.id = 'subtitle-content'
   statusbarContent.className = 'mega3d-subtitle-content'
   statusbarContainer.appendChild(statusbarContent)
 }
 
 export function showSubtitle(content: string, typeSpeed?: number) {
-  const typed = new Typed('#subtitle-content', {
+  const el = isMicApp ? window.rawWindow.document.body.querySelector('#subtitle-content') : document.querySelector('#subtitle-content')
+  const typed = new Typed(el, {
     strings: [content],
     typeSpeed,
     showCursor: false,
